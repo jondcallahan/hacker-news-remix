7d7518f1428fe9e1d87432409402c01bb0278206
Jon Callahan <j@joncallahan.com>
2022-02-13 10:09:37 -0600

    feat: adds a loading spinner

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
   useMatches,
+  useTransition,
 } from "remix";
 import type { MetaFunction } from "remix";
 import stylesUrl from "./styles/global.css";
@@ -45,6 +46,7 @@ function highlightFirstStoryLink(e: KeyboardEvent<HTMLBodyElement>) {
 
 export default function App() {
   const matches = useMatches();
+  const transition = useTransition();
   return (
     <html lang="en">
       <head>
@@ -86,6 +88,21 @@ export default function App() {
             </h5>
           </section>
         </nav>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="loading-spinner"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          data-loading={transition?.state === "loading"}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          />
+        </svg>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
