35a5bf8c67a85115889110c3b2970a8c568551d3
Jon Callahan <jon_callahan@intuit.com>
2022-07-19 13:10:20 -0500

    feat: display points and comments as tags

--- app/routes/__layout/index.tsx
+++ app/routes/__layout/index.tsx
@@ -4,7 +4,7 @@ import {
   useLoaderData,
   useNavigate,
 } from "@remix-run/react";
-import { getItem, getTopStories } from "~/utils/api.server";
+import { getTopStories } from "~/utils/api.server";
 import { getRelativeTimeString } from "~/utils/time";
 import {
   Box,
@@ -17,6 +17,10 @@ import {
   Container,
   Flex,
   Image,
+  chakra,
+  Tag,
+  TagLeftIcon,
+  TagLabel,
 } from "@chakra-ui/react";
 
 type StoryType = {
@@ -31,7 +35,7 @@ type StoryType = {
   url: string;
 };
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async () => {
   const timerStart = process.hrtime();
 
   const storiesPerPage = 30;
@@ -57,21 +61,15 @@ export default function Index() {
               key={story.id}
               borderRadius="lg"
               display={"grid"}
-              gridTemplateColumns="70px 1fr"
               _hover={{
                 boxShadow: "lg",
               }}
               transition="box-shadow 0.2s ease-in-out"
               backgroundColor="orange.50"
-              padding="2"
+              padding="4"
               width="full"
             >
-              <Grid placeContent="center">
-                <Stat>
-                  <StatNumber fontSize="xl">{story.score}</StatNumber>
-                </Stat>
-              </Grid>
-              <Grid gap="1">
+              <Grid gap="2">
                 {story.url && (
                   <Flex alignItems="center">
                     <Image
@@ -144,11 +142,71 @@ export default function Index() {
                 <ChakraLink
                   as={RemixLink}
                   to={`/item/${story.id}`}
-                  prefetch="intent"
+                  prefetch="render"
                   width="full"
-                  display="inline-block"
+                  display="flex"
+                  justifyContent="space-between"
+                  gap="2"
+                  role="group"
+                  _hover={{
+                    textDecoration: "none",
+                  }}
                 >
-                  {story.descendants || "0"} Comments
+                  <Tag size="lg">
+                    <TagLeftIcon
+                      as={() => (
+                        <chakra.svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          boxSize="5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M7 11l5-5m0 0l5 5m-5-5v12"
+                          />
+                        </chakra.svg>
+                      )}
+                    />
+                    <TagLabel>{story.score} points </TagLabel>
+                  </Tag>
+                  <Tag
+                    colorScheme="blue"
+                    borderWidth="2px"
+                    borderColor="transparent"
+                    size="lg"
+                    transition="border-color ease-in 0.17s"
+                    _groupHover={{
+                      borderStyle: "solid",
+                      borderColor: "blue.500",
+                    }}
+                  >
+                    <TagLeftIcon
+                      as={() => (
+                        <chakra.svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          boxSize="4"
+                          display="inline"
+                          verticalAlign="text-top"
+                          marginInlineEnd={1}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                          />
+                        </chakra.svg>
+                      )}
+                    />
+                    <TagLabel>{story.descendants || "0"} comments</TagLabel>
+                  </Tag>
                 </ChakraLink>
               </Grid>
             </Box>
