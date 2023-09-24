d20f0b70f0a793124a617961d6ae8e9833d4b12f
Jon Callahan <j@joncallahan.com>
2023-09-23 22:28:58 -0500

    chore: fix start command

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 const config = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildPath: "build",
-  devServerPort: 8002,
   ignoredRouteFiles: [".*"],
   serverDependenciesToBundle: ["react-tweet/api", "plaiceholder"],
 };
