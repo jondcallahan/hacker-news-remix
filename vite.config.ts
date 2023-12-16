2a8e77d9a44fe7da66fd7df08fdb3eece4bb4a01
Jon Callahan <j@joncallahan.com>
2023-12-16 16:48:53 -0600

    fix: build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import morgan from "morgan";
 import { defineConfig, type ViteDevServer } from "vite";
+
+installGlobals();
 
 export default defineConfig({
   plugins: [requestLogger(), remix()],
