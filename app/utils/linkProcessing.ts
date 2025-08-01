396e9a73c6dc3c6d05eefef979a44efe8ca5ebd7
Jon Callahan <j@joncallahan.com>
2025-08-01 21:31:26 +0100

    Add debugging to HN link replacement function

--- app/utils/linkProcessing.ts
+++ app/utils/linkProcessing.ts
@@ -7,8 +7,17 @@ export function replaceHnLinksWithReader(html: string): string {
   // - http://news.ycombinator.com/item?id=12345
   // - https://news.ycombinator.com/item?id=12345&other=param
   // - https://news.ycombinator.com/item?id=12345#comment
-  return html.replace(
+  
+  // Add debugging to see if function is being called
+  const result = html.replace(
     /https?:\/\/news\.ycombinator\.com\/item\?id=(\d+)(?:[&?#][^"\s<>]*)?/g,
     'https://hn.joncallahan.com/item/$1'
   );
+  
+  // Log when we find and replace HN links (only in development)
+  if (typeof window !== 'undefined' && html !== result) {
+    console.log('HN link replacement:', { original: html, replaced: result });
+  }
+  
+  return result;
 }
