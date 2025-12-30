bf1ba2acc695232ad52c5b009cb4f8d251728ce8
Jon Callahan <j@joncallahan.com>
2025-12-29 21:12:05 -0600

    fix: make handle and date text smaller on mobile

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -53,10 +53,10 @@ function TweetEmbed({ tweet }: { tweet: Tweet }) {
     >
       <Flex direction="column" justify={"center"} height="full" px={4} gap={[0.5, 1]}>
         <Flex alignItems="center" gap="2">
-          <Avatar src={avatar} name={handle} size="sm">
+          <Avatar src={avatar} name={handle} size={["xs", "sm"]}>
             {isVerified && <AvatarBadge boxSize="1em" bgColor="twitter.500" />}
           </Avatar>
-          <Text fontWeight="bold">{handle}</Text>
+          <Text fontWeight="bold" fontSize={["xs", "md"]}>{handle}</Text>
         </Flex>
         <Heading
           size={["xs", "md"]}
@@ -101,10 +101,10 @@ function TweetEmbed({ tweet }: { tweet: Tweet }) {
           </Flex>
         </Flex>
         <Flex alignItems="center" gap="2">
-          <Icon viewBox="0 0 20 20" boxSize={4} fill="twitter.600">
+          <Icon viewBox="0 0 20 20" boxSize={[3, 4]} fill="twitter.600">
             <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.615 11.615 0 006.29 1.84" />
           </Icon>
-          <Text>{friendlyDate}</Text>
+          <Text fontSize={["xs", "md"]}>{friendlyDate}</Text>
         </Flex>
       </Flex>
     </Box>
