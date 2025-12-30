6baae2ca8c694ec84941484db3da109d2834d4f5
Jon Callahan <j@joncallahan.com>
2025-12-29 21:06:19 -0600

    fix: improve tweet preview text fitting on mobile
    - Use smaller text size (xs) on mobile for tweet content
    - Allow 3 lines of text on mobile instead of 2
    - Reduce gap between elements on mobile
    - Hide photo indicator and engagement stats on mobile to save space

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -51,18 +51,23 @@ function TweetEmbed({ tweet }: { tweet: Tweet }) {
       bg="white"
       color="twitter.600"
     >
-      <Flex direction="column" justify={"center"} height="full" px={4} gap="1">
+      <Flex direction="column" justify={"center"} height="full" px={4} gap={[0.5, 1]}>
         <Flex alignItems="center" gap="2">
           <Avatar src={avatar} name={handle} size="sm">
             {isVerified && <AvatarBadge boxSize="1em" bgColor="twitter.500" />}
           </Avatar>
           <Text fontWeight="bold">{handle}</Text>
         </Flex>
-        <Heading size="md" lineHeight="shorter">
+        <Heading
+          size={["xs", "md"]}
+          lineHeight="shorter"
+          noOfLines={[3, 4]}
+          overflow="hidden"
+        >
           {friendlyText}
         </Heading>
         {hasImage && (
-          <Flex alignItems="center" gap="2">
+          <Flex alignItems="center" gap="2" display={["none", "flex"]}>
             <Icon
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
@@ -77,7 +82,7 @@ function TweetEmbed({ tweet }: { tweet: Tweet }) {
             <Text fontSize="sm">Photo attached</Text>
           </Flex>
         )}
-        <Flex alignItems="center" gap="2">
+        <Flex alignItems="center" gap="2" display={["none", "flex"]}>
           <Flex alignItems="center" gap="2">
             <Icon viewBox="0 0 20 20" fill="red.500" boxSize={5}>
               <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z" />
